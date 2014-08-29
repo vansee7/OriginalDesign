6b0d83fd70fd2ed3ce186154cdfa0988a79af9d8
unknown <vansee7@gmail.com>
2014-08-29 10:10:59 -0700

    Created urban sprawl.

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,41 @@
 void setup()
 {
-
+	background(135, 206, 250);
+	size(430, 400);
+	noLoop();
 }
+
+void house() 
+{
+	fill(128, 128, 128);
+	beginShape();
+	vertex(mouseX-20, mouseY+70);
+	vertex(mouseX-20, mouseY+30);
+	vertex(mouseX-40, mouseY+30);
+	vertex(mouseX, mouseY);
+	vertex(mouseX+40, mouseY+30);
+	vertex(mouseX+20, mouseY+30);
+	vertex(mouseX+20, mouseY+70);
+	vertex(mouseX-20, mouseY+70);
+	endShape();
+	noStroke();
+}
+
+void hills()
+{
+	noStroke();
+	fill(0, 128, 0);
+	triangle(0, 270, 300, 400, 0, 400);
+	ellipse(350, 350, 300, 400);
+}
+
 void draw()
 {
-  ellipse(50,50,50,50);
+	hills();
 }
 
+void mousePressed()
+{
+	house();
+	redraw();
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -16,13 +16,45 @@ public class OriginalDesign extends PApplet {
 
 public void setup()
 {
-
+	background(135, 206, 250);
+	size(430, 400);
+	noLoop();
 }
+
+public void house() 
+{
+	fill(128, 128, 128);
+	beginShape();
+	vertex(mouseX-20, mouseY+70);
+	vertex(mouseX-20, mouseY+30);
+	vertex(mouseX-40, mouseY+30);
+	vertex(mouseX, mouseY);
+	vertex(mouseX+40, mouseY+30);
+	vertex(mouseX+20, mouseY+30);
+	vertex(mouseX+20, mouseY+70);
+	vertex(mouseX-20, mouseY+70);
+	endShape();
+	noStroke();
+}
+
+public void hills()
+{
+	noStroke();
+	fill(0, 128, 0);
+	triangle(0, 270, 300, 400, 0, 400);
+	ellipse(350, 350, 300, 400);
+}
+
 public void draw()
 {
-  ellipse(50,50,50,50);
+	hills();
 }
 
+public void mousePressed()
+{
+	house();
+	redraw();
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
